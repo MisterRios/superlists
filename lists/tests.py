029066fc2b6571dc48da9793a0d2c19022e23581
misterrios <misterrios@gmail.com>
2015-01-28 14:54:48 +0100

    split up functional test into individual tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,10 @@
 from django.core.urlresolvers import resolve
-from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.test import TestCase
 
-from lists.views import home_page
 from lists.models import Item, List
+from lists.views import home_page
 
 
 class HomePageTest(TestCase):
@@ -20,9 +20,6 @@ class HomePageTest(TestCase):
         expected_html = (render_to_string('home.html'))
 
         self.assertEqual(response.content.decode(), expected_html)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.strip().endswith(b'</html>'))
 
 
 
